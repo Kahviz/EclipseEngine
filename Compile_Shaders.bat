8aa8f2847e5ae53495cf9f78321555af7a16d0a5
Kahviz <joakimukkonen@gmail.com>
2026-04-12 16:24:13 +0300

    Made easier to add to-be compiled vulkan shaders

--- Compile_Shaders.bat
+++ Compile_Shaders.bat
@@ -1,40 +1,36 @@
 @echo off
+setlocal enabledelayedexpansion
+
 echo Compiling Vulkan shaders
 
-REM Vertex shader
-if exist "Core\Shaders\Source\vertex.glsl" (
-    echo Compiling vertex.glsl...
-    "C:\VulkanSDK\1.4.335.0\Bin\glslangValidator.exe" -V "Core\Shaders\Source\vertex.glsl" -o "Core\Shaders\vertex.spv" --target-env vulkan1.2 -S vert
-    if errorlevel 1 (
-        echo ERROR: Failed to compile vertex shader
-        pause
-        exit /b 1
-    ) else (
-        echo vertex.spv created
-    )
-) else (
-    echo ERROR: vertex.glsl not found
-    dir Shaders\Source\
-    pause
-    exit /b 1
-)
+set "shaders[0]=vertex.glsl vert vertex.spv"
+set "shaders[1]=fragment.glsl frag fragment.spv"
 
-REM Fragment shader
-if exist "Core\Shaders\Source\fragment.glsl" (
-    echo Compiling fragment.glsl...
-    "C:\VulkanSDK\1.4.335.0\Bin\glslangValidator.exe" -V "Core\Shaders\Source\fragment.glsl" -o "Core\Shaders\fragment.spv" --target-env vulkan1.2 -S frag
-    if errorlevel 1 (
-        echo ERROR: Failed to compile fragment shader
-        pause
-        exit /b 1
-    ) else (
-        echo fragment.spv created
+set count=2
+
+for /l %%i in (0,1,%count%) do (
+    for /f "tokens=1,2,3" %%a in ("!shaders[%%i]!") do (
+        set "input=%%a"
+        set "type=%%b"
+        set "output=%%c"
+        
+        if exist "Core\Shaders\Source\!input!" (
+            echo Compiling !input!...
+            "C:\VulkanSDK\1.4.335.0\Bin\glslangValidator.exe" -V "Core\Shaders\Source\!input!" -o "Core\Shaders\!output!" --target-env vulkan1.2 -S !type!
+            if errorlevel 1 (
+                echo ERROR: Failed to compile !input!
+                pause
+                exit /b 1
+            ) else (
+                echo !output! created
+            )
+        ) else (
+            echo ERROR: !input! not found
+            dir Core\Shaders\Source\
+            pause
+            exit /b 1
+        )
     )
-) else (
-    echo ERROR: fragment.glsl not found
-    dir Shaders\Source\
-    pause
-    exit /b 1
 )
 
 echo Vulkan Shaders compiled successfully
